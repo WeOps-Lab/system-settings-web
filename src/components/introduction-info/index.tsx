b13f48bbcb2c13859f99d6b3e2ab7a0480bece96
chenlinggg <1489789468@qq.com>
2024-11-11 17:48:00 +0800

    optimization:拖拽功能优化，样式的优化

--- src/components/introduction-info/index.tsx
+++ src/components/introduction-info/index.tsx
@@ -8,8 +8,8 @@ interface IntroductionInfoProp {
 }
 const IntroductionInfo: React.FC<IntroductionInfoProp> = ({ message, title }) => (
   <div className={`h-16 w-full rounded-md ${introductionInfoStly.introductionInfo}`}>
-    <h1 className="text-xl ml-5 pt-2">{title}</h1>
-    <p className="ml-5">{message}</p>
+    <h1 className="text-base ml-5 pt-2">{title}</h1>
+    <p className="text-sm ml-5">{message}</p>
   </div>
 );
 
